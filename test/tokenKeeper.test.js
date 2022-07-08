64f09134c9ad6f624cb5cb438ae5739d8b7bfdd6
jowelynhornilla <jowelyn.hornilla@98labs.com>
2022-07-08 22:19:07 +0800

    feat: updated integration tests

--- test/tokenKeeper.test.js
+++ test/tokenKeeper.test.js
@@ -7,23 +7,44 @@ require("./utils");
 const TOKENS_OPENED = web3.utils.toWei("3");
 const TOKENS_LOCKED = web3.utils.toWei("5");
 
+
 contract("TokenKeeper", ([owner, alice, bob, random]) => {
 
     let tokenKeeper;
 
     beforeEach(async () => {
-        // TODO: check params
-        tokenKeeper = await TokenKeeper.new(owner, 10, alice);
+        // TODO: check minTimeFrame and tokenAddress
+        tokenKeeper = await TokenKeeper.new(owner, 165728454510, owner);
     });
 
     describe("Allocation Functionality", () => {
 
-        it("should allocate correct values", () => {
+        it("should not allocate tokens with invalid time frame", async () => {
+            const currentTime = await tokenKeeper.getNow();
+            const timeFrame = currentTime - 10;
+            
+            let Error;
+            try {
+                await tokenKeeper.allocateTokens(
+                    owner,
+                    TOKENS_OPENED,
+                    TOKENS_LOCKED,
+                    timeFrame
+                );
+            } catch (error) {
+                Error = error;
+            }
+            
+            assert.notEqual(Error, undefined);
+            assert.equal(Error.reason, "TokenKeeper: INVALID_TIME_FRAME");
+        });
+
+        it("should allocate correct values", async () => {
             const currentTime = await tokenKeeper.getNow();
             const timeFrame = currentTime + 10;
 
-            await tokenKeeper.allocateToken(
-                alice,
+            await tokenKeeper.allocateTokens(
+                owner,
                 TOKENS_OPENED,
                 TOKENS_LOCKED,
                 timeFrame
@@ -31,29 +52,12 @@ contract("TokenKeeper", ([owner, alice, bob, random]) => {
 
             const keeper = await tokenKeeper.keeperList.call(alice);
             const totalRequired = await tokenKeeper.totalRequired.call();
-            
-            assert.equal(keeper.keeperRate, TOKENS_LOCKED / timeFrame);
+
+            assert.equal(keeper.keeperRate, Math.trunc(TOKENS_LOCKED / timeFrame));
             assert.equal(keeper.keeperBalance, TOKENS_LOCKED % timeFrame + TOKENS_OPENED);
             assert.equal(keeper.keeperBalance, TOKENS_LOCKED % timeFrame + TOKENS_OPENED);
             assert.equal(totalRequired, TOKENS_OPENED + TOKENS_LOCKED);
 
-
-        });
-
-        it("should not allocate tokens with invalid time frame", () => {
-            const currentTime = await tokenKeeper.getNow();
-            const timeFrame = currentTime - 10;
-
-            await tokenKeeper.allocateToken(
-                alice,
-                TOKENS_OPENED,
-                TOKENS_LOCKED,
-                timeFrame
-            )
-
-            const keeper = await tokenKeeper.keeperList.call(alice);
-
-            assert.equal(keeper.keeperBalance, 0)
         });
     })
 
